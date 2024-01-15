53babd7b3c9dd0a15eda2c9bf319d40bb4a21e60
ImGhostCode <imghostcode@gmail.com>
2024-01-15 20:36:15 +0700

    implement soft delete

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
     constructor(configService: ConfigService) {
         super({
             datasources: {
@@ -12,5 +12,36 @@ export class PrismaService extends PrismaClient {
                 }
             }
         })
+
+        this.$use(async (params, next) => {
+            if (params.model == 'IrregularVerb' || params.model == 'Account' || params.model == 'Sentence' || params.model == 'User' || params.model == 'Word') {
+                if (params.action == 'delete') {
+                    // Delete queries
+                    // Change action to an update
+                    params.action = 'update'
+                    params.args['data'] = { isDeleted: true }
+                }
+                if (params.action == 'deleteMany') {
+                    // Delete many queries
+                    params.action = 'updateMany'
+                    if (params.args.data != undefined) {
+                        params.args.data['isDeleted'] = true
+                    } else {
+                        params.args['data'] = { isDeleted: true }
+                    }
+                }
+            }
+
+            // Continue with the operation
+            return next(params);
+        });
+    }
+
+    async onModuleInit() {
+        await this.$connect();
+    }
+
+    async onModuleDestroy() {
+        await this.$disconnect();
     }
 }
